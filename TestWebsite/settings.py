c5a250e601de847b465ade2dd1ee83b67b5b241b
nnminh171298 <ngocminh171298@gmail.com>
2018-02-13 22:03:23 +0200

    create the navbar

--- TestWebsite/settings.py
+++ TestWebsite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
 	'ngocminh9862.pythonanywhere.com',
+	'127.0.0.1',
 ]
 
 
--- TestWebsite/urls.py
+++ TestWebsite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('music/', include('Music.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
