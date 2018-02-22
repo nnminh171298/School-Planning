5010d9999acdba43f6aeae04b0708d711ae26c52
nnminh171298 <ngocminh171298@gmail.com>
2018-02-22 22:25:37 +0200

    Update, Delete albums, store media files

--- TestWebsite/urls.py
+++ TestWebsite/urls.py
@@ -17,9 +17,16 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Music.urls')),
     path('music/', include('Music.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
